10c70dff2855dfde5d3f321035e53a447b8969ce
Divakar-Murugesh <diva.srmvcas@gmail.com>
2017-07-13 13:16:55 +0530

    Examples added 1. Animation 2. Simple alert dialog with ListView

--- app/src/main/java/com/example/animations/AnimationExampleActivity.java
+++ app/src/main/java/com/example/animations/AnimationExampleActivity.java
@@ -1,9 +1,9 @@
 package com.example.animations;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.R;
@@ -15,21 +15,24 @@ public class AnimationExampleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
 
-        TextView textView1 = (TextView) findViewById(R.id.textView1);
-        Button button1 = (Button) findViewById(R.id.button1);
+        final LinearLayout linearLayout1 = (LinearLayout) findViewById(R.id.layout1);
+        final TextView textView1 = (TextView) findViewById(R.id.textView1);
+        final Button button1 = (Button) findViewById(R.id.button1);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            // Using hardware layer
-            textView1.animate().translationX(400).withLayer();
-        } else {
-            textView1.animate().translationX(400);
-        }
+        // Start Action
+        button1.animate().translationX(350).setDuration(1000).withStartAction(new Runnable() {
+            public void run() {
+                textView1.animate().translationX(500 - (textView1.getWidth() / 2)).setDuration(1000).withLayer();
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            // Using hardware layer
-            button1.animate().translationY(400).withLayer();
-        } else {
-            button1.animate().translationY(400);
-        }
+                // End Action
+                button1.animate().alpha(0).withEndAction(new Runnable() {
+                    public void run() {
+                        // Remove view from parent layout
+                        linearLayout1.removeView(button1);
+                    }
+                });
+            }
+        });
+
     }
 }
