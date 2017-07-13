960635001b17076563e8b5db9d672fb4461ee6ca
Divakar-Murugesh <diva.srmvcas@gmail.com>
2017-07-13 15:40:16 +0530

    Examples added 1. Animation 2. Simple alert dialog with ListView

--- app/src/main/java/com/example/animations/AnimationExampleActivity.java
+++ app/src/main/java/com/example/animations/AnimationExampleActivity.java
@@ -1,7 +1,11 @@
 package com.example.animations;
 
+import android.animation.LayoutTransition;
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -15,9 +19,15 @@ public class AnimationExampleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
 
+        LayoutTransition l = new LayoutTransition();
+        l.enableTransitionType(LayoutTransition.CHANGING);
         final LinearLayout linearLayout1 = (LinearLayout) findViewById(R.id.layout1);
+        linearLayout1.setLayoutTransition(l);
+
         final TextView textView1 = (TextView) findViewById(R.id.textView1);
         final Button button1 = (Button) findViewById(R.id.button1);
+        final Button button2 = (Button) findViewById(R.id.button2);
+        final Button button3 = (Button) findViewById(R.id.button3);
 
         // Start Action
         button1.animate().translationX(350).setDuration(1000).withStartAction(new Runnable() {
@@ -34,5 +44,26 @@ public class AnimationExampleActivity extends AppCompatActivity {
             }
         });
 
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                linearLayout1.addView(new Button(AnimationExampleActivity.this));
+
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(AnimationExampleActivity.this, SecondActivity.class);
+                ActivityOptions options = ActivityOptions.makeScaleUpAnimation(view, 0, 0, view.getWidth(), view.getHeight());
+                startActivity(intent, options.toBundle());
+
+            }
+        });
+
     }
+
 }
