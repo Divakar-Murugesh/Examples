a66c2e567f162cd0ed19889568a0f3f5f5b6757f
Divakar-Murugesh <diva.srmvcas@gmail.com>
2017-07-14 15:52:11 +0530

    Examples added 1. Animation 2. Simple alert dialog with ListView

--- app/src/main/java/com/example/MainActivity.java
+++ app/src/main/java/com/example/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,13 +19,15 @@ import com.example.simple_alert_dialog_with_list.SimpleAlertDialogActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int PERMISSIONS_CODE = 1001;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Array of strings...
-        String[] strings = {"Animations", "Simple Alert dialog with ListView", "Data passing between single activity to multiple fragments"};
+        String[] strings = {"Animations", "Simple Alert dialog with ListView", "Data passing between single activity to multiple fragments", "permission"};
 
         ListView listView1 = (ListView) findViewById(R.id.listView1);
 
@@ -38,9 +45,34 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(new Intent(MainActivity.this, SimpleAlertDialogActivity.class));
                 } else if (position == 2) {
                     startActivity(new Intent(MainActivity.this, TempActivity.class));
+                } else {
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION,}, PERMISSIONS_CODE);
+                    } else {
+                        // You can do you operation directly. If you added permissions in AndroidManifest...
+                        // Or visit following link to read about permission check.
+                        // https://stackoverflow.com/questions/7203668/how-permission-can-be-checked-at-runtime-without-throwing-securityexception
+                    }
                 }
 
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_CODE:
+                if (grantResults.length > 0 && grantResults[grantResults.length-1] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("permission", "accepted");
+                } else {
+                    Log.d("permission", "denied");
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                break;
+        }
     }
 }
